cdac43190bf8eca479b95c99d0b43fe8d429b1e7
tanabee <tanabee1986@gmail.com>
2020-11-01 09:35:28 +0900

    ビルド時に古い docs ファイルを一度全削除

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,8 +2,10 @@
 // https://github.com/gatsbyjs/gatsby/issues/18975#issuecomment-607329178
 const path = require('path');
 const fs = require('fs-extra');
+const rimraf = require('rimraf');
 
 exports.onPostBuild = () => {
+  rimraf.sync(path.join(__dirname, 'docs'));
   fs.copySync(path.join(__dirname, 'public'), path.join(__dirname, 'docs'), {
     overwrite: true,
   });
